faa923f4cbd9e775514892fcc8427a1ffb5dd226
Omar Rafat <omarrafat60@gmail.com>
2023-08-24 22:35:16 +0300

    Setup Swagger

--- main.go
+++ main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"log"
 	"os"
 
 	"github.com/Okira-E/go-as-your-backend/app/datasource"
 
-	"github.com/Okira-E/go-as-your-backend/app/routers"
+	"github.com/Okira-E/go-as-your-backend/app/routes"
 	"github.com/Okira-E/go-as-your-backend/app/utils"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name UNLICENSE
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
 func main() {
 	// -- Setup flags
 	migrate := false
@@ -64,17 +75,20 @@ func main() {
 		}))
 	}
 
-	// -- Setup routes
 	// Get the sql.DB object to pass to the routes
 	sqlDB, err := gormDB.DB()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	routers.SetupRoutes(app, sqlDB)
+	routes.SetupRoutes(app, sqlDB)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
 	// -- Start server
-	port := os.Getenv("PORT")
 	err = app.Listen(":" + port)
 	if err != nil {
 		log.Fatal(err)
