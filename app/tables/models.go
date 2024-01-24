f4c4c5683b101f0f48ef7989f7a956e30216bb80
Omar Rafat <omarrafat60@gmail.com>
2024-01-24 19:15:24 +0300

    Added more advanced examples of tables and relationships and constraints

--- app/tables/models.go
+++ app/tables/models.go
@@ -1,21 +1,58 @@
 package tables
 
 import (
-	"database/sql"
 	"time"
 )
 
-// SystemUsers is the model for the system_users table
-type systemUsers struct {
-	ID        uint `gorm:"primaryKey, autoIncrement"`
-	Name      string
-	Email     *string         `gorm:"unique"`
-	Salary    sql.NullFloat64 `gorm:"type:decimal(10,2)"`
-	Age       sql.NullInt32
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+// ADDED TABLES SHOULD BE ADDED TO app/datasource/datasource.go
+
+type Organizations struct {
+	ID          uint      `gorm:"primaryKey; autoIncrement" json:"id"`
+	Name        string    `gorm:"unique; not null" json:"name"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt   time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
 }
 
-var Models = []any{
-	&systemUsers{},
+type Products struct {
+	ID          uint      `gorm:"primaryKey; autoIncrement" json:"id"`
+	Name        string    `gorm:"unique; not null" json:"name"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt   time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
+}
+
+type Projects struct {
+	Product *Products
+
+	ID          uint   `gorm:"primaryKey; autoIncrement" json:"id"`
+	Name        string `gorm:"unique; not null" json:"name"`
+	Description string `json:"description"`
+	// Foreign key
+	ProductID uint      `json:"productId"`
+	Users     []*Users  `gorm:"many2many:users_projects" json:"users"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
+}
+
+type Users struct {
+	ID        uint        `gorm:"primaryKey; autoIncrement" json:"id"`
+	FirstName string      `gorm:"not null" json:"firstName"`
+	LastName  string      `gorm:"not null" json:"lastName"`
+	Email     string      `gorm:"not null" json:"email"`
+	Title     string      `gorm:"not null" json:"title"`
+	Password  string      `gorm:"not null" json:"password"`
+	Projects  []*Projects `gorm:"many2many:users_projects"`
+	Roles     []*Roles    `gorm:"many2many:users_roles"`
+	CreatedAt time.Time   `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt time.Time   `gorm:"autoUpdateTime" json:"updatedAt"`
+}
+
+type Roles struct {
+	ID          uint      `gorm:"primaryKey; autoIncrement" json:"id"`
+	Name        string    `gorm:"unique; not null" json:"name"`
+	Description string    `gorm:"not null" json:"description"`
+	Users       []*Users  `gorm:"many2many:users_roles"`
+	CreatedAt   time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt   time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
 }
